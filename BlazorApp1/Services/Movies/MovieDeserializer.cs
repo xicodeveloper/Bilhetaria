ceef70a7d488babddde6aa06b2558995c730b93f
xicodeveloper <forofrancisco4@gmail.com>
2025-03-13 00:03:53 +0000

    UI changes in the list of films

--- BlazorApp1/Services/Movies/MovieDeserializer.cs
+++ BlazorApp1/Services/Movies/MovieDeserializer.cs
@@ -1,22 +1,31 @@
 using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 
 namespace BlazorApp1.Services.Movies;
 
-public class MovieDeserializer
+public class MovieDeserializer : IDisposable
 {
+    private HttpClient? _httpClient;
+
     public MovieResponse FetchPopularMovies(string link)
     {
+        _httpClient = new HttpClient(); // Mantém uma instância reutilizável
+        
         var apiKey = Environment.GetEnvironmentVariable("API_KEY");
         if (string.IsNullOrEmpty(apiKey))
         {
             throw new InvalidOperationException("API key não configurada no ambiente.");
         }
-        using (var client = new HttpClient())
-        {
-            var endpoint = new Uri(link + "?api_key=" + apiKey);
-            var response = client.GetAsync(endpoint).Result;
-            string jsonString = response.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<MovieResponse>(jsonString);
-        }
+        
+        var endpoint = new Uri(link + "?api_key=" + apiKey);
+        var response = _httpClient.GetAsync(endpoint).Result;
+        string jsonString = response.Content.ReadAsStringAsync().Result;
+        return JsonConvert.DeserializeObject<MovieResponse>(jsonString);
+    }
+
+    public void Dispose()
+    {
+        _httpClient?.Dispose();
     }
 }
