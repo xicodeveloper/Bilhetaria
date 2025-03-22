e131c381e9578aa573f5fa8cfc8c2b1e2cd2de71
Rosa Nóbrega <95029751+quinhaz@users.noreply.github.com>
2025-03-22 18:15:50 +0000

    pastas bin/obj eliminadas

---
+++ es-a3-quinhaz-main/InfraSim2/InfraSim2/Services/UserCounter.cs
@@ -0,0 +1,23 @@
+// controlo do contador e notifica mudanças
+ 
+using System;
+using System.Threading.Tasks;
+
+public class UserCounter
+{
+    public int Counter { get; private set; } = 0;
+
+
+    // avisa mudanças
+    public event Action? OnCounterChanged;
+
+    public async Task StartIncrementing(int maxValue, int step, int delay)
+    {
+        for (int i = 0; i < maxValue; i += step)
+        {
+            Counter += step;
+            OnCounterChanged?.Invoke(); // Notifica a UI que o valor mudou
+            await Task.Delay(delay);
+        }
+    }
+}
