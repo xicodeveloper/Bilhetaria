0a73f9e987d9cd17a53bf530713b363995050ed6
xicodeveloper <forofrancisco4@gmail.com>
2025-04-15 12:31:13 +0100

    Homepage filme

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -62,7 +62,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<VerificationService>();
 builder.Services.AddScoped<EmailService>();  // Registrar o EmailService
 
-
+builder.Services.AddHttpClient();
 builder.Services.AddHttpContextAccessor();
 
 // 5. Configuração do Blazor
