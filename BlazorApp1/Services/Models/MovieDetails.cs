db0bd065955dbf6bcf7d5997af46e5a03ad991ac
xicodeveloper <forofrancisco4@gmail.com>
2025-04-19 17:03:18 +0100

    Front end movie especifico

--- BlazorApp1/Services/Models/MovieDetails.cs
+++ BlazorApp1/Services/Models/MovieDetails.cs
@@ -6,9 +6,10 @@ public class MovieDetails
     public string Title { get; set; }
     public string OriginalTitle { get; set; }
     public string Overview { get; set; }
+    public string poster_path { get; set; }
     public string Tagline { get; set; }
     public int Runtime { get; set; }
     public double VoteAverage { get; set; }
-    public string BackdropPath { get; set; }
+    public string backdrop_path { get; set; }
     public List<Genre> Genres { get; set; }
 }
